d2a8f386c445e259921288eb4a7d2de1e26142f9
Mehdi Davoodi <mahdidavoodi7@gmail.com>
2025-12-14 15:35:18 +0330

    fix: update primary blur intensity constant

--- constants/camera.ts
+++ constants/camera.ts
@@ -3,5 +3,5 @@
 // ------------------------------------------------------
 
 export const CAMERA_ZOOM = 0.1;
-export const BLUR_INTENSITY_PRIMARY = 50;
+export const BLUR_INTENSITY_PRIMARY = 100;
 export const BLUR_INTENSITY_SECONDARY = 20;
